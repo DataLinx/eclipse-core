707de2cd5d9ca14e7e0792bf469c6d3da79259a4
Omer Sabic <omer@sabic.si>
2019-12-26 20:51:52 +0100

    Add files, UI dep

---
+++ src/Console/Commands/PostComposerInstall.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Ocelot\Core\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class PostComposerInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ocelot:post-composer-install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run the post composer install procedure';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "Running Ocelot's Post Composer install procedure..." . PHP_EOL;
+
+        // Copy the .env file
+        // ------------------
+        if (!file_exists('.env')) {
+            if (copy('vendor/ocelot/core/.env.example', '.env')) {
+                echo ".env.example copied to .env" . PHP_EOL;
+                Artisan::call("key:generate --ansi");
+            } else {
+                die("Could not copy .env.example to .env");
+            }
+        } else {
+            echo ".env already exists" . PHP_EOL;
+        }
+    }
+}
---
+++ src/Providers/CoreServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ocelot\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Ocelot\Core\Console\Commands\PostComposerInstall;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+       if ($this->app->runningInConsole()) {
+           $this->commands([
+               PostComposerInstall::class,
+           ]);
+       }
+    }
+}
