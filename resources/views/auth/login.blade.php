bead16bdc6d092868abf93eb26f4310fb4fdf6d6
Omer Šabić <omer@datalinx.si>
2022-08-14 11:22:17 +0200

    OC-87 Upgrade login view to use components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,36 +12,11 @@
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
-                            <div class="mb-3">
-                                <label for="email" class="form-label">{{ _('E-mail address') }}</label>
+                            <x-form::input type="email" name="email" label="{{ _('E-mail address') }}" required autocomplete="email" autofocus />
 
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <x-form::input type="password" name="password" label="{{ _('Password') }}" required autocomplete="current-password" />
 
-                                @error('email')
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="password" class="form-label">{{ _('Password') }}</label>
-
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="mb-3 form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="form-check-label" for="remember">
-                                    {{ _('Remember Me') }}
-                                </label>
-                            </div>
+                            <x-form::switcher name="remember" label="{{ _('Remember me') }}" />
 
                             <div class="d-grid">
 
