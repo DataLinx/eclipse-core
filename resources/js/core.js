7dc2adacfce20b644e39e450ea6822003eca92c2
Omer Šabić <omer@datalinx.si>
2024-02-29 20:11:17 +0100

    refactor: wip tailwind replacement for bootstrap

--- resources/js/core.js
+++ resources/js/core.js
@@ -1,27 +1,3 @@
-import './init';
+import './bootstrap.js';
 
 import '../sass/core.scss';
-
-/*
-$('.user-delete').click(function(){
-    if (confirm('Are you sure you want to delete this user?')) {
-        var $row = $(this).closest('tr');
-        $.ajax('/users/'+ $(this).data('id'), {
-            method: 'delete',
-            data: {
-                '_token': $('meta[name="csrf-token"]').attr('content'),
-            },
-            success: function(data) {
-                if ( ! data.error) {
-                    $row.remove();
-                }
-            }
-        })
-    }
-});
-*/
-
-_.forEach(document.getElementsByClassName('toast'), function(element) {
-    const toast = new bootstrap.Toast(element);
-    toast.show();
-});
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,36 +3,34 @@
     <x-slot name="header"></x-slot>
 
     <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">{{ _('Login') }}</div>
+        <div class="justify-items-center">
+            <div class="card">
+                <div class="card-header">{{ _('Login') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                            <x-form::input type="email" name="email" label="{{ _('E-mail address') }}" required autocomplete="email" autofocus />
+                        <x-form::input type="email" name="email" label="{{ _('E-mail address') }}" required autocomplete="email" autofocus />
 
-                            <x-form::input type="password" name="password" label="{{ _('Password') }}" required autocomplete="current-password" />
+                        <x-form::input type="password" name="password" label="{{ _('Password') }}" required autocomplete="current-password" />
 
-                            <x-form::switcher name="remember" label="{{ _('Remember me') }}" />
+                        <x-form::switcher name="remember" label="{{ _('Remember me') }}" />
 
-                            <div class="d-grid">
+                        <div class="d-grid">
 
-                                <button type="submit" class="btn btn-primary">
-                                    {{ _('Login') }}
-                                </button>
+                            <button type="submit" class="btn btn-primary">
+                                {{ _('Login') }}
+                            </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ _('Forgot your password?') }}
-                                    </a>
-                                @endif
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ _('Forgot your password?') }}
+                                </a>
+                            @endif
 
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
